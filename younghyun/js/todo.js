ab9f910bf8133c58ef29c4f696984617bfea5ee6
EumYoungHyun <opwer032@naver.com>
2021-03-24 01:09:07 +0900

    feat: Test createElement function
    make li

--- younghyun/js/todo.js
+++ younghyun/js/todo.js
@@ -1,5 +1,6 @@
 const todoForm = document.querySelector(".todoForm"),
-  inputText = todoForm.querySelector(".inputText");
+  inputText = todoForm.querySelector(".inputText"),
+  listForm = document.querySelector(".todoList");
 
 const todoList = [];
 
@@ -8,7 +9,11 @@ function handleSubmit(event) {
   const content = inputText.value;
   todoList.splice(0, 0, content);
   inputText.value = "";
-  console.log(todoList);
+  const li = document.createElement("li");
+  const text = document.createElement("p");
+  text.innerText = content;
+  li.appendChild(text);
+  listForm.appendChild(li);
 }
 
 function init() {
