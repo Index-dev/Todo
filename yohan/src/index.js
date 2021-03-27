87b26f0a5ba8661004111f1a08457aaf4be1b2b8
yohan Moon <munyohan@naver.com>
2021-03-27 09:56:00 +0900

    feat: Make overall layout and some functions

---
+++ yohan/src/index.js
@@ -0,0 +1,27 @@
+const inputTodo = document.querySelector('.input-todo');
+const btnAdd = document.querySelector('.btn-add');
+const todoListBox = document.querySelector('.todo-list-box');
+const todoList = [];
+
+inputTodo.addEventListener('keydown', e => {
+  inputTodo.textContent = e.target.value;
+});
+
+inputTodo.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    addTodo(inputTodo.textContent);
+    console.log(todoList);
+  }
+});
+
+btnAdd.addEventListener('click', function () {
+  addTodo(inputTodo.textContent);
+  console.log(todoList);
+});
+
+function addTodo(value) {
+  todoList.push(value);
+  inputTodo.value = '';
+
+  const html = ``;
+}
