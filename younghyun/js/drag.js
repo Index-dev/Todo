c68a03afc6d7b19e25b6c856abe13a71c23a4dfc
Eum <opwer032@naver.com>
2021-03-30 20:36:52 +0900

    feat: Make drag & drops
    it only order changes.
    needed to add array order change (+ session storage array)

--- younghyun/js/drag.js
+++ younghyun/js/drag.js
@@ -13,7 +13,31 @@ draggables.forEach((draggable) => {
 containers.forEach((container) => {
     container.addEventListener("dragover", (event) => {
         event.preventDefault();
+        const afterElement = getDragAfterElement(container, event.clientY);
         const draggedli = document.querySelector(".dragging");
-        container.appendChild(draggedli);
+        if (afterElement === undefined) {
+            container.appendChild(draggedli);
+        } else {
+            container.insertBefore(draggedli, afterElement);
+        }
     });
 });
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [
+        ...container.querySelectorAll(".todoItem:not(.dragging)"),
+    ];
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+        } else {
+            return closest;
+        }
+    }, closestInitialValue()).element;
+}
+
+function closestInitialValue() {
+    return { offset: Number.NEGATIVE_INFINITY };
+}
