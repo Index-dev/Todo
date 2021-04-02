d69039c6be02fcafb5e6c01e9b105d5891994961
EumYoungHyun <opwer032@naver.com>
2021-04-03 00:15:38 +0900

    feat: Add shift function
    Add shift function between important and common list

--- younghyun/js/drag.js
+++ younghyun/js/drag.js
@@ -3,43 +3,56 @@ const containers = document.querySelectorAll(".todoContainer");
 containers.forEach((container) => {
   container.addEventListener("dragover", (event) => {
     event.preventDefault();
-    const afterElement = getDragAfterElement(container, event.clientY);
     const draggedli = document.querySelector(".dragging");
+    const ulType = container.classList.contains("important")
+      ? "important"
+      : "common";
+    const liType = draggedli.classList.contains("important")
+      ? "important"
+      : "common";
+
+    if (ulType !== liType) {
+      draggedli.classList.remove(liType);
+      draggedli.classList.add(ulType);
+    }
+
+    const afterElement = getDragAfterElement(container, event.clientY);
 
     if (afterElement === undefined) {
       container.appendChild(draggedli);
     } else {
       container.insertBefore(draggedli, afterElement);
     }
-
-    let newImportants = [];
-    let newCommons = [];
-    document.querySelectorAll(".todoItem").forEach(function (item) {
-      if (item.classList[2] === "important") {
-        newImportants.push({
-          index: parseInt(item.id),
-          content: item.querySelector("p").innerText,
-          done: Boolean(item.classList[1]),
-          importance: item.classList[2],
-        });
-      } else {
-        newCommons.push({
-          index: parseInt(item.id),
-          content: item.querySelector("p").innerText,
-          done: Boolean(item.classList[1]),
-          importance: item.classList[2],
-        });
-      }
-    });
-
-    saveList(newImportants, newCommons);
-    function saveList(importants, commons) {
-      localStorage.setItem("Importants", JSON.stringify(importants));
-      localStorage.setItem("Commons", JSON.stringify(commons));
-    }
+    FixSessionList();
   });
 });
+function FixSessionList() {
+  let newCommons = [];
+  let newImportants = [];
+  document.querySelectorAll(".todoItem").forEach(function (item) {
+    if (item.classList.contains("important")) {
+      newImportants.push({
+        index: parseInt(item.id),
+        content: item.querySelector("p").innerText,
+        done: Boolean(item.classList.contains("true") ? "true" : "false"),
+        importance: "important",
+      });
+    } else {
+      newCommons.push({
+        index: parseInt(item.id),
+        content: item.querySelector("p").innerText,
+        done: Boolean(item.classList.contains("true") ? "true" : "false"),
+        importance: "common",
+      });
+    }
+  });
 
+  saveList(newImportants, newCommons);
+  function saveList(importants, commons) {
+    localStorage.setItem("Importants", JSON.stringify(importants));
+    localStorage.setItem("Commons", JSON.stringify(commons));
+  }
+}
 function getDragAfterElement(container, y) {
   const draggableElements = [
     ...container.querySelectorAll(".todoItem:not(.dragging)"),
